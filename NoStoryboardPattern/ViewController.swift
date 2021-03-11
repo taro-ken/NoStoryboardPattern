66bdb62f1e37dcf5680556dd4712b63e7b978897
kentaro kimoto <exiler2411k@gmail.com>
2021-03-11 22:24:39 +0900

    first commit

---
+++ NoStoryboardPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoStoryboardPattern
+//
+//  Created by 木元健太郎 on 2021/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
